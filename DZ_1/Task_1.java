b0d3406880c50b6c50d377e7118231abc52224f3
KumalyakovaAS <explay1355@mail.ru>
2023-06-28 22:23:24 +0300

    ДЗ 1

---
+++ DZ_1/Task_1.java
@@ -0,0 +1,30 @@
+package DZ_1;
+
+import java.util.Scanner;
+
+/**
+ * Вычислить n-ое треугольного число (сумма чисел от 1 до n), 
+ * n! (произведение чисел от 1 до n)
+ */
+public class Task_1 {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Введите число n: ");
+        Integer n = scanner.nextInt();
+        int triangle_number =  (n*(n+1))/2;
+        System.out.println(n + "-ое" + " треугольное число " + triangle_number);
+        int factorial = 1;
+        if (n == 1){
+            System.out.println(factorial + "- факториал числа " + n);
+        } 
+        else{
+            for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial + "- факториал числа " + n);
+        }
+        
+    }
+
+    
+}
